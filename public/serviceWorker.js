eca600395101c0a64c2a740551bf8437d9c59f35
Roberto Nobre <robertonobre90@gmail.com>
2020-06-17 20:36:00 -0300

    Adding service worker

---
+++ public/serviceWorker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = [ 'index.html', 'offline.html' ];
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+
+});
+
+// Active the SW 
+self.addEventListener('activate', (event) => {
+
+});
