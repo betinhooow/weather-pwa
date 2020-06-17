3f2c87855c6909c7804553d7437b13ad7a6cdccc
Roberto Nobre <robertonobre90@gmail.com>
2020-06-16 23:23:14 -0300

    Form page, search on enter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, KeyboardEvent } from 'react';
 import './App.css';
+import { fetchWather } from './api/fetchWeather';
 
 const App = () => {
+  const [query, setQuery] = useState('');
+
+  const search = async (e: KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter'){
+      const data = await fetchWather(query);
+
+      console.log(data)
+    }
+  }
+
   return (
-    <h1>App</h1>
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+    </div>
   )
 }
 
