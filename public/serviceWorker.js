d7abae693aaa6c134aa6b2706ee9bd1a1ed10562
Roberto Nobre <robertonobre90@gmail.com>
2020-06-17 21:59:24 -0300

    Deleting old cache and keeping new one

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,32 +1,42 @@
-const CACHE_NAME = 'version-1';
-const urlsToCache = [ 'index.html', 'offline.html' ];
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
 
 const self = this;
 
 // Install SW
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log('Opened cache');
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
 
-            return cache.addAll(urlsToCache);
-        })
-    )
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 // Listen for requests
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(() => {
-        return fetch(event.request)
-          .catch(() => caches.match('offline.html'))
-      })
-  )
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
 });
 
-// Active the SW 
-self.addEventListener('activate', (event) => {
+// Active the SW
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
 
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
 });
